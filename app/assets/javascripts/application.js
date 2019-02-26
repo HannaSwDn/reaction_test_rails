f819cf035577403044264df5647445a237ca8f69
hanna_nyman <hannanyman.sk@gmail.com>
2019-02-26 22:38:36 +0100

    Changes background color of the game div to red after a random amount of seconds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,12 +5,21 @@
 
 const startGame = () => {
     // generate random time until game page goes red
+    let secondsUntilRed = Math.floor((Math.random() * 7 + 2) * 1000)
+
+    // create a variable to hold the game page
+    let background = document.getElementById('test')
 
     // change game page background color to red when time is up
+    setTimeout(() => {
+        background.style.backgroundColor = 'red'
+    }, secondsUntilRed);
 
     // start a timer when time is up
 
     // count the time from when the div turns red until user clicks on screen
+
+    // create a variable to hold the paragraph to show the score
     
     // display the time in the paragraph on the game page
 }
