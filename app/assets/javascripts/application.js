f8a231c241b0ad3d32f3baf5fc0a08f3a7111ae7
hanna_nyman <hannanyman.sk@gmail.com>
2019-02-26 22:18:15 +0100

    Adds startGame function with support comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,16 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+const startGame = () => {
+    // generate random time until game page goes red
+
+    // change game page background color to red when time is up
+
+    // start a timer when time is up
+
+    // count the time from when the div turns red until user clicks on screen
+    
+    // display the time in the paragraph on the game page
+}
