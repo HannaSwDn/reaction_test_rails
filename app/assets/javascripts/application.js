844a2ec7f6caee99f64bfcae00813e1467a6ffc7
hanna_nyman <hannanyman.sk@gmail.com>
2019-02-28 22:18:29 +0100

    Removes unnecessary comments from script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,30 +7,23 @@
 let startTime
 let circle
 
-// set DOM variables when document has loaded
 document.addEventListener('turbolinks:load', () => {
-    // create a variable to hold the game page
     circle = document.getElementById('circle')
     
     circle.addEventListener('click', () => {
-        // count the time from when the div turns red until user clicks on screen
         let currentDate = new Date()
         let currentTime = currentDate.getTime()
         let endTime = currentTime - startTime
     
-        // create a variable to hold the paragraph to show the score
         let displayReactionTime = document.getElementById('paragraph')
         
-        // display the time in the paragraph on the game page
         displayReactionTime.innerHTML = `Your reaction time was: ${endTime / 1000} seconds`
     })
 })
 
 const startGame = () => {
-    // generate random time until game page goes red
     let secondsUntilRed = Math.floor((Math.random() * 7 + 2) * 1000)
 
-    // change game page background color to red when time is up
     setTimeout(() => {
         circle.style.backgroundColor = 'red'
         let currentDate = new Date()
